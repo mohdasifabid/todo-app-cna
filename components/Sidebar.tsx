dc5dec80a37c4b06957ec77f1d9043db564bef94
Mohd Asif <masifabid@outlook.com>
2024-02-22 21:02:33 +0530

    Refactor Sidebar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,17 +1,22 @@
+
 export default function Sidebar() {
-  const btnsNameList = ["Add Task", "Search", "Today", "Upcoming", "Labels"];
   return (
     <div className="bg-white w-1/5 min-h-screen flex flex-col justify-start pt-10">
-      {btnsNameList?.map((btnName, index) => {
-        return (
-          <p
-            key={index}
-            className="p-2 pl-5 justify-start cursor-pointer hover:bg-gray-100 ml-2 mr-2 rounded-md"
-          >
-            {btnName}
-          </p>
-        );
-      })}
+      <a className="p-2 pl-5 justify-start cursor-pointer hover:bg-gray-100 ml-2 mr-2 rounded-md">
+        Add Task
+      </a>
+      <a className="p-2 pl-5 justify-start cursor-pointer hover:bg-gray-100 ml-2 mr-2 rounded-md">
+        Search
+      </a>
+      <a className="p-2 pl-5 justify-start cursor-pointer hover:bg-gray-100 ml-2 mr-2 rounded-md">
+        Today
+      </a>
+      <a className="p-2 pl-5 justify-start cursor-pointer hover:bg-gray-100 ml-2 mr-2 rounded-md">
+        Upcoming
+      </a>
+      <a className="p-2 pl-5 justify-start cursor-pointer hover:bg-gray-100 ml-2 mr-2 rounded-md">
+        Labels
+      </a>
     </div>
   );
 }
---
+++ components/addTask.tsx
@@ -0,0 +1,8 @@
+export default function AddTask () {
+    return (
+        <div className="w-1/4 bg-gray-100">
+            <p className="text-4xl">Task</p>
+
+        </div>
+    )
+}
