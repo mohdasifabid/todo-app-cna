1874c8c85aadb984f8b861d18abdfa937101cbb5
Mohd Asif <masifabid@outlook.com>
2024-02-25 14:31:04 +0530

    Feat delete todo functionality using useMutation

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,9 +1,21 @@
 import { Props } from "@/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
 
 export const TodoCard = (props: Props) => {
   const { setIsModalOpen, data } = props;
-  const { title, content, label } = data;
+  const { title, content, label, _id } = data;
 
+  const queryClient = useQueryClient();
+  const deleteTodoHandler = async () => {
+    const endPoint = `https://todo-app-be-u8g0.onrender.com/api/todos/${_id}`;
+    const response = await axios.delete(endPoint);
+    return response;
+  };
+  const { isPending, error, mutate } = useMutation({
+    mutationFn: deleteTodoHandler,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["todos"] }),
+  });
   return (
     <div className="border-2 border-solid border-black  p-3 flex justify-between">
       <div>
@@ -20,7 +32,10 @@ export const TodoCard = (props: Props) => {
         >
           Edit
         </button>
-        <button className="border pt-1 pl-3 pr-3 pb-1 bg-black text-white rounded-md">
+        <button
+          className="border pt-1 pl-3 pr-3 pb-1 bg-black text-white rounded-md"
+          onClick={() => mutate()}
+        >
           Delete
         </button>
       </div>
